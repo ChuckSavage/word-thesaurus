03f0dbdcccd6eb9fa8fcafbf59681d3f230fd050
Chuck Savage <chuck@searisen.com>
2017-05-10 15:50:16 -0600

    empty credentials file

---
+++ word-thesaurus/ThisAddIn_Credentials.cs
@@ -0,0 +1,16 @@
+﻿namespace word_thesaurus
+{
+	public partial class ThisAddIn
+	{
+		/*
+         * The following uid & key need to be gotten from
+         *     http://www.abbreviations.com/api.php
+         *     
+         * Their email will contain your uid and token, the token is the key
+         *     User ID: NNNN
+         *     Token: ZZZZZZZZZZZ
+         */
+		private string uid = "NNNN"; // Numeric id
+		private string key = "XXXXXXXXXXXXXX"; // Looks like fEJNjkm5mHNWE7LWs
+	}
+}
